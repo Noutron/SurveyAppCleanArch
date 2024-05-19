b00d79fdd3c515c67ae73e0a6180dad71969455d
Velican ERDOĞAN <velicanerdogan@ticimax.com>
2024-05-20 01:06:44 +0300

    initial

---
+++ Application/Interfaces/ISurveyAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces;
+
+public interface ISurveyAppDbContext
+{
+     DbSet<Domain.Models.Survey> Surveys { get; set; }
+     DbSet<Option> Options { get; set; }
+     DbSet<Vote> Votes { get; set; }
+     
+     Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+}
---
+++ Domain/Models/Option.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public class Option : BaseModel
+{
+    public string Type { get; set; }
+    public string Description { get; set; }
+    public int Order { get; set; }
+    
+    public virtual Survey Survey { get; set; }
+    public virtual List<Vote> Votes { get; set; }
+}
